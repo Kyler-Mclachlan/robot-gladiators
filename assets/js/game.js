c81d05ad6b495d8167172416a9e9298faa89ef3b
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-11-29 13:04:29 -0600

    added while condition to for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,7 +23,7 @@ var enemyAttack = 10;
 var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
-
+    while(enemyHealth > 0){
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
     if (promptFight === "fight" || promptFight === "FIGHT") {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -65,10 +65,13 @@ var fight = function(enemyName) {
       } else {
         window.alert("You need to choose a valid option. Try again!");
       }
+    }
 };
 
 //calling the function//
 
 for(var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
